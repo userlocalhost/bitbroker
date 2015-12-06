1d6e8b2d5f46c5f07bb611598b66bf71cb7404be
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2015-12-06 20:15:19 +0900

    make interface of BitBroker::Broker class in advance for test

---
+++ lib/bitbroker/broker.rb
@@ -0,0 +1,12 @@
+module BitBroker
+  # This method communicate with AMQP for transmitting and receiving data
+  class Broker
+    def send data
+      ### No implementation
+    end
+
+    def receive
+      ### No implementation
+    end
+  end
+end
