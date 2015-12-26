8b50c493df6efd2e5c3249b3cbcab0ec438b4e51
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2015-12-26 15:01:08 +0900

    add an unit-test of broker

---
+++ spec/broker_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe BitBroker::Broker do
+  context "subscriber" do
+    it "send data correctly"
+  end
+
+  context "publisher" do
+    it "receive data correctly"
+    it "couldn't receive data with invalid key"
+  end
+end
